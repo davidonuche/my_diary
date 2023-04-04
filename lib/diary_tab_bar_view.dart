bf56294a68d69a1d3c782f5f306d4ec1acc6ccda
David onuche <110064207+Davidbeebest@users.noreply.github.com>
2023-04-04 20:55:42 +0100

    show bottom sheet

--- lib/diary_tab_bar_view.dart
+++ lib/diary_tab_bar_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 import 'screens/diary_screen.dart';
 import 'screens/settings_screen.dart';
@@ -13,18 +14,60 @@ class DiaryTabBarView extends StatefulWidget {
 class _DiaryTabBarViewState extends State<DiaryTabBarView> {
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(length: 2,
-     child: Scaffold(
-      appBar: AppBar(
-        bottom: TabBar(tabs: [
-          Tab(icon: Icon(Icons.event_note)),
-          Tab(icon: Icon(Icons.settings)),
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          actions: [
+            IconButton(
+                onPressed: () {
+                  Get.bottomSheet(BottomSheet(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(30),
+                              topRight: Radius.circular(30))),
+                      enableDrag: false,
+                      builder: (context) {
+                        return Padding(
+                          padding: const EdgeInsets.all(18.0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              TextField(
+                                onChanged: (value) {
+                                  //TODO:- Change entry text
+                                },
+                              ),
+                              SizedBox(height: 8),
+                              ElevatedButton(
+                                  onPressed: () {
+                                    //TODO:- Add new entry
+                                    // Closes bottom sheet
+                                    Get.back();
+                                  },
+                                  child: Text("Add")),
+                            ],
+                          ),
+                        );
+                      },
+                      onClosing: () {}));
+                },
+                icon: Icon(
+                  Icons.add,
+                  color: Colors.white,
+                  size: 30,
+                )),
+          ],
+          bottom: TabBar(tabs: [
+            Tab(icon: Icon(Icons.event_note)),
+            Tab(icon: Icon(Icons.settings)),
+          ]),
+        ),
+        body: TabBarView(children: [
+          DiaryScreen(),
+          SettingsScreen(),
         ]),
       ),
-      body: TabBarView(children: [
-        DiaryScreen(),
-        SettingsScreen(),
-      ]),
-     ),);
+    );
   }
 }
--- lib/screens/diary_screen.dart
+++ lib/screens/diary_screen.dart
@@ -6,7 +6,23 @@ class DiaryScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('Diary Screen'),
+      child: ListView.separated(
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Date here"),
+                    SizedBox(height: 5),
+                    Text("Content here"),
+                  ]),
+            );
+          },
+          separatorBuilder: (context, index) => Divider(
+                height: 5,
+              ),
+          itemCount: 4),
     );
   }
 }
